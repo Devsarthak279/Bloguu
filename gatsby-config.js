8d1014f83e5af31abb1b02dde875f32ca5eaa3fa
Devsarthak279 <nucleosarthak279@gmail.com>
2024-08-15 16:45:33 +0530

    blog project updated

---
+++ gatsby-config.js
@@ -0,0 +1,69 @@
+const GOOGLE_ANALYTICS_TRACKING_ID = 'UA-65599459-9';
+
+module.exports = {
+  pathPrefix: '/',
+  siteMetadata: {
+    title: 'BRIIM - A Machine Learning in JavaScript Movement',
+    description: 'BRIIM is a movement to enable JavaScript enthusiasts and web developers in machine learning. Learn about artificial intelligence and data science, two fields which are governed by machine learning, in JavaScript. Take it right to your browser with WebGL.',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: GOOGLE_ANALYTICS_TRACKING_ID,
+        anonymize: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: 'https://28ded14b3083446ea38ffb62b5caf958@sentry.io/260428',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "gatsby-movement",
+        short_name: "gatsby-movement",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#ffffff",
+        display: "minimal-ui",
+        icons: [
+          {
+            // Everything in /static will be copied to an equivalent
+            // directory in /public during development and build, so
+            // assuming your favicons are in /static/favicons,
+            // you can reference them here
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-256x256.png`,
+            sizes: `256x256`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
+  ],
+};
